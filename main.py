438b9e417db1992f23bae2c2f964b5c143ac3673
Vannesa <140695617+vannesa0wens@users.noreply.github.com>
2024-03-27 16:39:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def sum(a, b, c, d):
 a = int(input('Enter onest number: '))
 b = int(input('Enter twond number: '))
 
-print(f'Sum of {a} and {b} is {sum(a, b, c, e)}')
+print(f'Sum of {a} and {b} is {sum(a, b, c, d)}')
